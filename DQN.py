0faddd21cfdc560be646239408b6d1ed35b3f1e9
miniluz <javiermiladossantos@gmail.com>
2025-05-30 18:34:52 +0200

    Update model, act, DQNagent init, DQN, replay buffer
    Co-authored-by: ClaudiaOviedoGovantes <ClaudiaOviedoGovantes@users.noreply.github.com>

--- DQN.py
+++ DQN.py
@@ -1,22 +1,35 @@
-# import numpy as np
-# import random
-# import time
+import numpy as np
+import random
+import time
 
-# import torch
-# import tensorflow as tf
+import tensorflow as tf
 
 from collections import deque
 
 from lunar import LunarLanderEnv
+
+import os
+os.environ["KERAS_BACKEND"] = "tensorflow"
+
+import keras
 
 # Lecturas interesantes: 
 # https://www.cs.toronto.edu/~vmnih/docs/dqn.pdf (Playing atari with DQN)
 # https://www.nature.com/articles/nature14236 (Human level control through RL)
 # https://www.lesswrong.com/posts/kyvCNgx9oAwJCuevo/deep-q-networks-explained
 
-class DQN(torch.nn.Module/tf.keras.Model):
+class DQN(keras.Model):
     def __init__(self, state_size, action_size, hidden_size):
-        pass
+        super().__init__()
+
+        self.dense1 = keras.layers.Dense(hidden_size, activation=keras.activations.leaky_relu)
+        self.dense2 = keras.layers.Dense(hidden_size, activation=keras.activations.leaky_relu)
+        self.outputLayer = keras.layers.Dense(action_size, activation=keras.activations.leaky_relu)
+
+    def call(self, inputs):
+        x = self.dense1(inputs)
+        x = self.dense2(x)
+        return self.outputLayer(x)
     
     #puede requerir mas funciones segun la libreria escogida.
     
@@ -25,12 +38,10 @@ class ReplayBuffer():
         self.buffer = deque(maxlen=buffer_size) # deque es una doble cola que permite añadir y quitar elementos de ambos extremos
 
     def push(self, state, action, reward, next_state, done):
-        # insert into buffer
-        pass
+        self.buffer.append((state, action, reward, next_state, done))
         
     def sample(self, batch_size):
-        # get a batch of experiences from the buffer
-        pass
+        return random.sample(self.buffer, batch_size)
     
     def __len__(self):
         return len(self.buffer)
@@ -84,22 +95,24 @@ class DQNAgent():
         # de entrada igual al espacio de observaciones
         # y un numero de salida igual al espacio de acciones.
         # Asi como un numero de capas intermedias adecuadas.
+        HIDDEN_SIZE = 64
+
         self.q_network = DQN(
             state_size=observation_space.shape[0],
             action_size=action_space.n,
-            hidden_size=0 #elegir un tamaño de capa oculta
+            hidden_size=HIDDEN_SIZE #elegir un tamaño de capa oculta
         )
         
         self.target_network = DQN(
             state_size=observation_space.shape[0],
             action_size=action_space.n,
-            hidden_size=0 #elegir un tamaño de capa oculta
+            hidden_size=HIDDEN_SIZE #elegir un tamaño de capa oculta
         )
         
         # Set weights of target network to be the same as those of the q network
-        self.target_network.
+        self.target_network.set_weights(self.q_network.get_weights())
       
-        self.optimizer = # depende del framework que uses (tf o pytorch)
+        self.optimizer = keras.optimizers.Adam(learning_rate=0.01)# depende del framework que uses (tf o pytorch)
         
         print(f"QNetwork:\n {self.q_network}")
           
@@ -109,10 +122,23 @@ class DQNAgent():
         it can be randomly sampled from the action space (based on epsilon) or
         it can be the action with the highest Q-value from the model.
         """
-        pass
+        state = self.lunar.state
+
+        if (random.uniform(0,1) <= self.epsilon):
+            # With probability epsilon, choose a random action
+            action = self.lunar.env.action_space.sample()
+
+        else:
+            # With probability 1 - epsilon
+            # Use q-network to evaluate q(s,a) for all actions
+            state_tensor = np.array([state], dtype=np.float32)
+            result = self.q_network(state_tensor)[0]
+
+            # Choose the action with the highest q(s,a)
+            action = np.argmax(result)
+
     
         next_state, reward, done = self.lunar.take_action(action, verbose=False)
-        
         return next_state, reward, done, action
     
     def update_model(self):
@@ -123,7 +149,33 @@ class DQNAgent():
         """
         
         states, actions, rewards, next_states, dones = self.memory.sample(self.batch_size)
+
+        states = tf.convert_to_tensor(states, dtype=tf.float32)
+        actions = tf.convert_to_tensor(actions, dtype=tf.int32)
+        rewards = tf.convert_to_tensor(rewards, dtype=tf.float32)
+        next_states = tf.convert_to_tensor(next_states, dtype=tf.float32)
+        dones = tf.convert_to_tensor(dones, dtype=tf.float32)
+
+        with tf.GradientTape() as tape:
+            # Calculate q(s,a) according to the q-network
+            # We use one-hot and reduce-sum to keep only the q for the selected action
+            #   [ A, B, C, D ] * [ 0, 0, 1, 0 ] -> [ 0, 0, C, 0 ]
+            #   reduce_sum([0, 0, C, 0]) -> 0 + 0 + C + 0 = C
+            q_values = tf.reduce_sum(self.q_network(states) * tf.one_hot(actions, 4), axis=1)
+            
+            # Get maximum q-value according to target network for the next state
+            # And use it to approximate the next state
+            next_q_values = tf.reduce_max(self.target_network(next_states), axis=1)
+            
+            # Calculate the expected q-values, only adding the next value if done exists
+            expected_q_values = rewards + self.gamma * next_q_values * (1 - dones)
+            
+            loss = tf.keras.losses.MeanSquaredError()(expected_q_values, q_values)
         
+        # Train using the gradients
+        gradients = tape.gradient(loss, self.q_network.trainable_weights)
+        self.optimizer.apply_gradients(zip(gradients, self.q_network.trainable_weights))
+
         return loss
         
     def update_target_network(self):
